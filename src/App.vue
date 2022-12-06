136e20ee2db97c05617bb815945b828a8ba5b462
ellielle <40385743+ellielle@users.noreply.github.com>
2022-12-06 14:03:41 -0500

    finish solution

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
-<script setup>
-import CountdownHeader from '@/components/CountdownHeader.vue'
-import CountdownSegment from './components/CountdownSegment.vue'
-import { useNow } from '@vueuse/core'
+<script setup lang="ts">
+import CountdownHeader from "@/components/CountdownHeader.vue";
+import { ref, Ref, watch } from "vue";
+import CountdownSegment from "./components/CountdownSegment.vue";
+import { useNow } from "@vueuse/core";
 
-const now = useNow()
-const christmas = new Date('12/25/2022 00:00:00')
+interface TimeRemaining {
+  daysRemaining: number;
+  hoursRemaining: number;
+  minutesRemaining: number;
+  secondsRemaining: number;
+}
+
+const timeRemaining = ref<TimeRemaining>({
+  daysRemaining: 0,
+  hoursRemaining: 0,
+  minutesRemaining: 0,
+  secondsRemaining: 0
+});
+
+const now = useNow();
+const christmas = new Date("12/25/2022 00:00:00");
+
+const getTimeToChristmas = () => {
+  const timeLeft = christmas.getTime() - now.value.getTime();
+  timeRemaining.value.daysRemaining =
+    Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  timeRemaining.value.hoursRemaining =
+    Math.floor(timeLeft % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+  timeRemaining.value.minutesRemaining =
+    Math.floor(timeLeft % (1000 * 60 * 60) / (1000 * 60));
+  timeRemaining.value.secondsRemaining =
+    Math.floor(timeLeft % (1000 * 60) / 1000);
+};
+watch(now, getTimeToChristmas);
 </script>
 <template>
   <div class="w-full h-full flex justify-center items-center p-10">
@@ -12,14 +40,19 @@ const christmas = new Date('12/25/2022 00:00:00')
       <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
         <CountdownHeader />
         <main class="flex justify-center">
-          <CountdownSegment label="days" number="00" />
-          <CountdownSegment label="hours" number="00" />
-          <CountdownSegment label="minutes" number="00" />
-          <CountdownSegment label="seconds" number="00" />
+          <CountdownSegment label="days"
+            :number="timeRemaining.daysRemaining" />
+          <CountdownSegment label="hours"
+            :number="timeRemaining.hoursRemaining" />
+          <CountdownSegment label="minutes"
+            :number="timeRemaining.minutesRemaining" />
+          <CountdownSegment label="seconds"
+            :number="timeRemaining.secondsRemaining" />
         </main>
       </div>
       <h4 class="mt-10 text-gray-400 text-center text-sm">
-        This challenge brought to you by <a href="https://vueschool.io/" class="underline">Vue School</a>
+        This challenge brought to you by <a href="https://vueschool.io/"
+        class="underline">Vue School</a>
       </h4>
     </div>
   </div>
--- src/components/CountdownSegment.vue
+++ src/components/CountdownSegment.vue
@@ -1,13 +1,25 @@
-<script setup>
-defineProps({
-  label: String,
-  number: Number,
-})
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  number: number
+}>();
+
+const numberToText = (countdownNumber: number) => {
+  if (countdownNumber <= 10) {
+    return countdownNumber.toString().padStart(2, "0");
+  }
+  return countdownNumber.toString();
+};
+
 </script>
 <template>
   <div class="segment text-center">
     <div class="pt-10 overflow-hidden relative">
-      <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+      <Transition>
+      <span :key="number" class="numbers text-green absolute top-0 left-[50%]">
+        {{ numberToText(number) }}
+      </span>
+      </Transition>
     </div>
 
     <span class="label block pt-2">{{ label }}</span>
@@ -17,11 +29,26 @@ defineProps({
 .segment {
   width: 80px;
 }
+
 .numbers {
   transform: translateX(-50%);
   font-size: 32px;
 }
+
 .label {
   font-size: 16px;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+}
+
+.v-enter-from {
+  transform: translateX(-50%) translateY(100%);
+}
+
+.v-leave-to {
+  transform: translateX(-50%) translateY(-100%);
 }
 </style>
