6d9abce63635cda6e4dc30e308d6f03efc0ed3f3
ellielle <40385743+ellielle@users.noreply.github.com>
2022-12-07 15:27:48 -0500

    finish solution

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-# Advent Of Vue 2022
-
-## If you didn't get here from an email, read this section! Skip it otherwise.
-
-[Advent Of Vue](https://adventofvue.com) is a series of 24 Vue coding challenges that are sent out every day from December 1 to December 24 via [a dedicated newsletter](https://www.getrevue.co/profile/AdventOfVue). If you'd like to receive more of these puzzles in the future, go ahead and sign up!
+# Solution to day 5
 
 ## Problem description
 
@@ -11,7 +7,7 @@ It's time to gift presents! And every present needs a label. So let's create our
 - Make the static `TO` and `FROM` fields dynamic so we can enter custom names.
 - Below the label, add two input fields with two-way binding for the `TO` and `FROM` fields.
 - When entering data into the input fields, the `TO` and `FROM` fields should dynamically update.
-- Hint: you can look at the `./src/App.spec.js` file to see a specification of the desired outcome.
+- Hint: you can look at the `./src/App.spec.ts` file to see a specification of the desired outcome.
 - Hint: you can run the tests with `npm run test` to see if your solution matches the specification.
 
 ## Example of a solution in action
--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { ref } from "vue";
+
+const toField = ref("");
+const fromField = ref("");
+</script>
+
 <template>
   <div class="w-full h-full flex justify-center items-center">
     <div>
@@ -6,15 +13,31 @@
           <img src="/assets/santa.svg" alt="" class="max-w-[12em]" />
           <div>
             <h1 class="font-serif text-4xl">Happy Holidays!</h1>
-            <div class="mt-4 bg-white text-black p-2 rounded">TO: John</div>
-            <div class="mt-2 bg-white text-black p-2 rounded">FROM: Joana</div>
+            <div class="mt-4 bg-white text-black p-2 rounded">
+              TO: {{ toField }}
+            </div>
+            <div class="mt-2 bg-white text-black p-2 rounded">
+              FROM: {{ fromField }}
+            </div>
           </div>
         </div>
+      </div>
+      <div class="mt-4">
+        <label for="input-to" class="mr-3">To:</label>
+        <input type="text"
+          name="input-to"
+          id="input-to"
+          class="bg-gray-100 border-2 border-gray-200 rounded"
+          v-model="toField">
+      </div>
+      <div class="mt-4">
+        <label for="input-from" class="mr-3">From:</label>
+        <input type="text"
+          name="input-from"
+          id="input-from"
+          class="bg-gray-100 border-2 border-gray-200 rounded focus:ring-0"
+          v-model="fromField">
       </div>
     </div>
   </div>
 </template>
-
-<script setup>
-// ...
-</script>
