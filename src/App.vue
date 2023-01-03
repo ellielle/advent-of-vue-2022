64a57681f67681747f8be62f1b69da1de24ec3f6
ellielle <40385743+ellielle@users.noreply.github.com>
2023-01-03 11:46:08 -0500

    convert to ts

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
+<script lang="ts">
+
+</script>
+
 <template>
   <div class="w-full h-full flex justify-center items-center">
-    <span class="text-3xl">Good luck!</span>
+    <span class="w-11 h-11"></span>
   </div>
 </template>
+
+<style scoped>
+.w-11 {
+  border: 1px solid black;
+}
+</style>
